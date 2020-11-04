477d864523b6041fb128a33561c1ed6308395ab2
brunacmelo <brunacmelo14@gmail.com>
2020-11-04 17:52:35 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {CourseComponent} from "./course/course.component";
+import {CourseResolver} from "./services/courses.resolver";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
+
+    },
+    {
+        path: 'courses/:id',
+        component: CourseComponent,
+        resolve: {
+            course: CourseResolver
+        }
+    },
+    {
+        path: "**",
+        redirectTo: '/'
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
